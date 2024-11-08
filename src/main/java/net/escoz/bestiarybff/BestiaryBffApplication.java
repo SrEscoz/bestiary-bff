ca55e26b5c6b7c03c3942ac40dd0670e9cdabeef
Escoz <kyrovilches@gmail.com>
2024-11-08 21:59:50 +0100

    Commit inicial

---
+++ src/main/java/net/escoz/bestiarybff/BestiaryBffApplication.java
@@ -0,0 +1,13 @@
+package net.escoz.bestiarybff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestiaryBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestiaryBffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/escoz/bestiarybff/BestiaryBffApplicationTests.java
@@ -0,0 +1,13 @@
+package net.escoz.bestiarybff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestiaryBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
